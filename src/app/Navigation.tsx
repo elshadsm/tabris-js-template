cd0ee468e4b36ccff4c9e41e174f5970f2bf8ccb
Elshad Seyidmammadov <elshad@eclipsesource.com>
2020-07-30 16:55:58 +0400

    Introduce Navigation

---
+++ src/app/Navigation.tsx
@@ -0,0 +1,26 @@
+import { NavigationView, Page, WidgetCollection } from 'tabris';
+import { shared } from 'tabris-decorators';
+
+@shared
+export default class Navigation {
+
+  private navigationView: NavigationView = (
+    <NavigationView
+      stretch
+      drawerActionVisible />
+  );
+
+  navigateTo(target: object) {
+    if (!(target instanceof Page)) {
+      throw new Error('Unknown target');
+    }
+    const pageStack = this.navigationView.pages().toArray();
+    const pageIndex = pageStack.indexOf(target);
+    if (pageIndex !== -1) {
+      new WidgetCollection(pageStack.slice(pageIndex + 1)).dispose();
+    } else {
+      this.navigationView.append(target);
+    }
+  }
+
+}
